865ba75bc669559d76e26852479c0072c8813a7d
luciensadi <luciensadi@gmail.com>
2017-06-22 09:03:14 -0700

    Update github_config.cpp
    Added trailing semicolon.

--- src/github_config.cpp
+++ src/github_config.cpp
@@ -1,2 +1,2 @@
-const char *github_issues_url = "https://api.github.com/repos/luciensadi/AwakeMUD/issues"
+const char *github_issues_url = "https://api.github.com/repos/luciensadi/AwakeMUD/issues";
 const char *github_authentication = "";
