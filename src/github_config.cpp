22b8674f8e262eb0d597dc7b2779ddff178d3ea1
Lucien Sadi <luciensadi@gmail.com>
2017-06-22 09:54:25 -0700

    Removed actual config file, added template. This helps ensure people won't accidentally check in their creds.

--- src/github_config.cpp
+++
@@ -1,2 +0,0 @@
-const char *github_issues_url = "https://api.github.com/repos/luciensadi/AwakeMUD/issues";
-const char *github_authentication = "";
---
+++ src/github_config_template.cpp
@@ -0,0 +1,6 @@
+/* The moment you make any changes to this file, move it from 
+    github_config_templates.cpp to github_config.cpp. This 
+    ensures that your credentials will not be checked in to GitHub. */
+
+const char *github_issues_url = "";
+const char *github_authentication = "";
