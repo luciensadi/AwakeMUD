b0c5462eab2c3066c90eb567c379b021104564c2
jalles76 <jan@jalles.org>
2017-06-23 19:33:12 +0200

    Fixed missing clothing items and added shoes to chargen

--- src/chargen.cpp
+++ src/chargen.cpp
@@ -692,7 +692,9 @@ void points_menu(struct descriptor_data *d)
 void create_parse(struct descriptor_data *d, const char *arg)
 {
   int i = MIN(120, atoi(arg)), ok;
-  long shirts[5] = { 38072, 16218, 1024, 28462, 22680 }, pants[5] = { 1023, 39828, 17954, 38009, 22683 };
+  long shirts[5] = { 70000, 70001, 70002, 70003, 70004 };
+  long pants[5] = { 70005, 70006, 70007, 70008, 70009 };
+  long shoes[5] = { 70010, 70011, 70012, 70013, 70014};
 
   switch (d->ccr.mode)
   {
@@ -939,6 +941,9 @@ void create_parse(struct descriptor_data *d, const char *arg)
       obj_to_char(read_object(2041, VIRTUAL), d->character);
     GET_EQ(d->character, WEAR_BODY) = read_object(shirts[number(0, 4)], VIRTUAL);
     GET_EQ(d->character, WEAR_LEGS) = read_object(pants[number(0, 4)], VIRTUAL);
+    GET_EQ(d->character, WEAR_FEET) = read_object(shoes[number(0, 4)], VIRTUAL);
+    if (real_object(1815) > -1)
+      obj_to_char(read_object(1815, VIRTUAL), d->character);
     ccr_type_menu(d);
     break;
   case CCR_TOTEM:
