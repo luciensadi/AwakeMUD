f843a360000dea5b9bcf312778c5bdc21cf0e092
Vodur <clayton.richey@gmail.com>
2019-08-03 05:44:17 +0000

    luai auto prints results

--- src/awlua_luai.cpp
+++ src/awlua_luai.cpp
@@ -9,6 +9,8 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
+#include <sstream>
+
 
 ACMD(do_luai)
 {
@@ -25,8 +27,6 @@ ACMD(do_luai)
     int top = lua_gettop(LS);
 
     lua_newtable(LS);
-    lua_pushliteral(LS, "");
-    lua_setfield(LS, -2, "input");
     ch->desc->ref_luai.Save(-1);
 
     send_to_char(ch, "Entered lua interpreter mode.\r\n"
@@ -57,6 +57,31 @@ static int incomplete (lua_State *L, int status) {
     return 0;  /* else... */
 }
 
+static int L_luai_print(lua_State *LS)
+{
+    std::ostringstream oss;
+    int n = lua_gettop(LS);
+    lua_getglobal(LS, "tostring");
+    for (int i = 1; i <= n; ++i)
+    {
+        const char *s;
+        size_t l;
+        lua_pushvalue(LS, -1); // function to be called
+        lua_pushvalue(LS, i);
+        lua_call(LS, 1, 1);
+        s = lua_tolstring(LS, -1, &l); // get result
+        if (s == NULL)
+            return luaL_error(LS, "'tostring' must return a string to 'luaiprint'");
+        if (i > 1) oss << ", ";
+        oss << s;
+        lua_pop(LS, 1);
+    }
+    oss << "\r\n";
+    std::string str = oss.str();
+    lua_pushlstring(LS, str.c_str(), str.size());
+    return 1;
+}
+
 void awlua::luai_handle(descriptor_data *d, const char *comm)
 {
     if (!strcmp(comm, "@"))
@@ -73,25 +98,69 @@ void awlua::luai_handle(descriptor_data *d, const char *comm)
     // Add input to any previous lines
     d->ref_luai.Push();
     int ind_luai = lua_gettop(LS);
+    bool first_line = false;
 
     lua_getfield(LS, -1, "input");
-    lua_pushliteral(LS, "\n");
-    lua_pushstring(LS, comm);
-    lua_concat(LS, 3);
-
+    if (lua_isnil(LS, -1))
+    {
+        first_line = true;
+        lua_pop(LS, 1);
+        lua_pushstring(LS, comm);
+    }
+    else
+    {
+        lua_pushliteral(LS, "\n");
+        lua_pushstring(LS, comm);
+        lua_concat(LS, 3);
+    }
+    
     int ind_all_input = lua_gettop(LS);
 
     // Try to load it
     size_t len;
     const char *all_input = lua_tolstring(LS, ind_all_input, &len);
-    int status = luaL_loadbuffer(LS, all_input, len, "=luai");
+    int status = -1;
+
+    if (first_line)
+    {
+        const char *ret_line = lua_pushfstring(LS, "return %s;", all_input);
+        status = luaL_loadbuffer(LS, ret_line, strlen(ret_line), "=luai");
+        if (status == LUA_OK)
+        {
+            lua_remove(LS, -2); // remove modified line
+        }
+        else
+        {
+            lua_pop(LS, 2); // pop result from 'luaL_loadbuffer' and modified line
+        }
+    }
+    if (status != LUA_OK)
+    {
+        status = luaL_loadbuffer(LS, all_input, len, "=luai");
+    }
     if (status == LUA_OK)
     {
+        const int ref_top = lua_gettop(LS) - 1;
         status = lua_pcall(LS, 0, LUA_MULTRET, 0);
         if (status == LUA_OK)
         {
-            // TODO: print anything on stack
-            lua_pushliteral(LS, "");
+            const int nrtn = lua_gettop(LS) - ref_top;
+            lua_pushcfunction(LS, L_luai_print);
+            lua_insert(LS, -1 - nrtn);
+            int rc = lua_pcall(LS, nrtn, 1, 0);
+            if (rc != LUA_OK)
+            {
+                SEND_TO_Q("Error in luai_print: ", d);
+                SEND_TO_Q(lua_tostring(LS, -1), d);
+                lua_pop(LS, 1);
+            }
+            else
+            {
+                SEND_TO_Q(lua_tostring(LS, -1), d);
+                lua_pop(LS, 1);
+            }
+
+            lua_pushnil(LS);
             lua_setfield(LS, ind_luai, "input");
             lua_settop(LS, top);
             return;
@@ -101,7 +170,7 @@ void awlua::luai_handle(descriptor_data *d, const char *comm)
             // print error
             SEND_TO_Q(luaL_checkstring(LS, -1), d);
             lua_pop(LS, 1); // pop error
-            lua_pushliteral(LS, "");
+            lua_pushnil(LS);
             lua_setfield(LS, ind_luai, "input");
             lua_settop(LS, top);
             return;
@@ -119,7 +188,7 @@ void awlua::luai_handle(descriptor_data *d, const char *comm)
         /* cannot or should not try to add continuation line */
         SEND_TO_Q(luaL_checkstring(LS, -1), d);
         lua_pop(LS, 1); // pop error
-        lua_pushliteral(LS, "");
+        lua_pushnil(LS);
         lua_setfield(LS, ind_luai, "input");
         lua_settop(LS, top);
         return;
@@ -138,7 +207,7 @@ const char *awlua::luai_get_prompt(descriptor_data *d)
     d->ref_luai.Push();
     lua_getfield(LS, -1, "input");
 
-    if (!strcmp(luaL_checkstring(LS, -1), ""))
+    if (lua_isnil(LS, -1))
     {
         rtn = "lua> ";
     }
