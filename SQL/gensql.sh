ac059e261dd23e6b26d96a05752e972c52b1bea4
Lucien Sadi <luciensadi@gmail.com>
2020-12-01 16:18:43 -0800

    Helpfile import.

--- SQL/gensql.sh
+++ SQL/gensql.sh
@@ -99,6 +99,11 @@ if [ -f "mail_fixes.sql" ]; then
   cat mail_fixes.sql >> gen_temp.sql
 fi
 
+if [ -f "helpfile_expansion.sql" ]; then
+  echo "" >> gen_temp.sql
+  cat helpfile_expansion.sql >> gen_temp.sql
+fi
+
 echo "You may preview the actions that are about to be taken by viewing gen_temp.sql now."
 echo ""
 echo "WARNING: IF YOUR DATABASE ALREADY EXISTS, THIS WILL PURGE IT."
