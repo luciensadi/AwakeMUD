074d485dd00177ce9dbe11f0b11141d88ab06874
Lucien Sadi <luciensadi@gmail.com>
2021-02-16 07:53:33 -0800

    Typo fixes for spec proc.

--- src/spec_procs.cpp
+++ src/spec_procs.cpp
@@ -5809,13 +5809,13 @@ SPECIAL(fatcop) {
   const char *fatcop_messages[] = {
     "Why are we stuck on guard duty? I'll tell you why, because we're the best of the best, the cream of the crop.",
     "Rumour is the higher-ups are keeping what happened here a secret, gotta be careful about the media snooping around.",
-    "These donuts are carbs and sugar, pure energy, energy i'll need if I gotta chase down any of these low-life criminals.",
+    "These donuts are carbs and sugar, pure energy, energy I'll need if I gotta chase down any of these low-life criminals.",
     "Back in my day, we shot first and didn't ask questions after. This new generation is too soft I tell ya.",
     "You see all those burned bodies they dragged outta here? Something real fragged up happened, I tell ya.",
-    "Look, all i'm sayin' is, if I wasn't supposed to eat three dozen donuts a day, it'd say so on the box, right?",
+    "Look, all I'm sayin' is, if I wasn't supposed to eat three dozen donuts a day, it'd say so on the box, right?",
     "So the doctor says... Rectum? Damn near killed em!",
     "Did I tell you about the time I took out a whole team of runners, single-handed, With no back-up?",
-    "Look, it ain't a crime if there ain't no witnesses, that's all i'm sayin'.",
+    "Look, it ain't a crime if there ain't no witnesses, that's all I'm sayin'.",
     "Did you see those chummers in black suits? Their badges didn't even have names on em, don't even know what agency."
   };
 #define NUM_FATCOP_MESSAGES 10
