c6e794c6319c1521aa5f830be140d520c08c6f34
Lucien Sadi <luciensadi@gmail.com>
2021-06-09 13:44:30 -0700

    Fixed a bug with idle deletion that was referencing character idnum instead of last login date.

--- src/newdb.cpp
+++ src/newdb.cpp
@@ -2165,7 +2165,7 @@ void idle_delete()
 #ifndef IDLEDELETE_DRYRUN
 		// TODO: Increase idle deletion leniency time by their TKE.
     int tke = atoi(row[2]);
-    time_t lastd = atol(row[0]);
+    time_t lastd = atoi(row[1]);
     if (lastd < (time(0) - (SECS_PER_REAL_DAY * (50 + (tke / 5))))) {
       DeleteChar(atol(row[0]));
       deleted++;
