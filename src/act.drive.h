5e16b23b4e37f1975ba27fbaf223e9fbd20e9835
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-20 05:30:15 +0800

    Delete src directory

--- src/act.drive.h
+++
@@ -1,7 +0,0 @@
-#ifndef _drive_h_
-#define _drive_h_
-
-void do_raw_ram(struct char_data *ch, struct veh_data *veh, struct veh_data *tveh, struct char_data *vict);
-void do_raw_target(struct char_data *ch, struct veh_data *veh, struct veh_data *tveh, struct char_data *vict, bool modeall, struct obj_data *obj);
-
-#endif
--- src/pocketsec.h
+++
@@ -1,6 +0,0 @@
-#ifndef _pocketsec_h_
-#define _pocketsec_h_
-
-void wire_nuyen(struct char_data *ch, struct char_data *targ, int amount, long isfile);
-
-#endif
