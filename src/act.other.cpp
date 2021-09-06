44edc27eb8430f0b49d32b0af95f585f71bd2f46
Lucien Sadi <luciensadi@gmail.com>
2021-09-06 15:59:38 -0700

    Fixed the shop unpack crashbug.

--- src/act.other.cpp
+++ src/act.other.cpp
@@ -3579,15 +3579,15 @@ ACMD(do_unpack)
       if (GET_OBJ_TYPE(shop) == ITEM_WORKSHOP && GET_WORKSHOP_GRADE(shop) == TYPE_WORKSHOP)
         break;
     }
+    
+    if (!shop) {
+      send_to_char(ch, "There is no workshop here to set up.\r\n");
+      return;
+    }
   }
   
   if (GET_OBJ_TYPE(shop) != ITEM_WORKSHOP || GET_WORKSHOP_GRADE(shop) != TYPE_WORKSHOP) {
     send_to_char(ch, "You can't unpack %s.\r\n", GET_OBJ_NAME(shop));
-    return;
-  }
-  
-  if (!shop) {
-    send_to_char(ch, "There is no workshop here to set up.\r\n");
     return;
   }
   
