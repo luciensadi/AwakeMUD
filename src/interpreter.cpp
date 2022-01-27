768ec3c53b5bdf6ca99d2e0468ad284bca1ac1e1
Lucien <luciensadi@gmail.com>
2022-01-27 12:47:22 -0800

    Made the spec debug line darker to be less spammy.

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -2133,14 +2133,14 @@ int special(struct char_data * ch, int cmd, char *arg)
         if (GET_MOB_SPEC(k) != NULL) {
           bool spec_returned_true = GET_MOB_SPEC(k) (ch, k, cmd, arg);
           if (IS_SENATOR(ch) && PRF_FLAGGED(ch, PRF_ROLLS))
-            send_to_char(ch, "Evaluation of spec for %s returned %s.\r\n", GET_CHAR_NAME(k), spec_returned_true ? "TRUE" : "FALSE");
+            send_to_char(ch, "^LEvaluation of spec for %s returned %s.^n\r\n", GET_CHAR_NAME(k), spec_returned_true ? "TRUE" : "FALSE");
           if (spec_returned_true)
             return 1;
         }
         if (mob_index[GET_MOB_RNUM(k)].sfunc != NULL) {
           bool spec_returned_true = GET_MOB_SPEC(k) (ch, k, cmd, arg);
           if (IS_SENATOR(ch) && PRF_FLAGGED(ch, PRF_ROLLS))
-            send_to_char(ch, "Evaluation of secondary spec for %s returned %s.\r\n", GET_CHAR_NAME(k), spec_returned_true ? "TRUE" : "FALSE");
+            send_to_char(ch, "^LEvaluation of secondary spec for %s returned %s.^n\r\n", GET_CHAR_NAME(k), spec_returned_true ? "TRUE" : "FALSE");
           if ((mob_index[GET_MOB_RNUM(k)].sfunc) (ch, k, cmd, arg))
             return 1;
         }
