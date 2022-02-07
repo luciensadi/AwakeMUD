7f4b103914059e9c6c2f07f3eed6e92f6bddd6af
Nappist <ablogqwer@gmail.com>
2022-02-07 13:10:20 -0800

    Update act.offensive.cpp

--- src/act.offensive.cpp
+++ src/act.offensive.cpp
@@ -598,18 +598,20 @@ bool passed_flee_success_check(struct char_data *ch) {
   if (!can_hurt(ch, FIGHTING(ch), TRUE, 0))
     return TRUE;
   
-  switch (GET_RACE(ch)) {
-    case RACE_SATYR:
-      racial_flee_modifier--;
-      break;
-    case RACE_DWARF:
-    case RACE_GNOME:
-    case RACE_MENEHUNE:
-      racial_flee_modifier++;
-      break;
-    if success_test(GET_QUI(ch), GET_QUI(FIGHTING(ch)) + racial_flee_modifier) > 0;
+    switch (GET_RACE(ch)) {
+      case RACE_SATYR:
+        racial_flee_modifier--;
+        break;
+      case RACE_DWARF:
+      case RACE_GNOME:
+      case RACE_MENEHUNE:
+        racial_flee_modifier++;
+        break;
+    }
+  
+    if (success_test(GET_QUI(ch), GET_QUI(FIGHTING(ch)) + racial_flee_modifier) > 0) {
       return TRUE;
-  }
+    }
 }
 
 ACMD(do_flee)
