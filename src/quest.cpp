5d6ea4bdd3bd79c337163662551b0be1cca15a62
Lucien <luciensadi@gmail.com>
2022-02-13 08:25:16 -0800

    Fixed crashbug during build of quests.

--- src/quest.cpp
+++ src/quest.cpp
@@ -1460,7 +1460,21 @@ void reboot_quest(int rnum, struct quest_data *quest)
   if (quest_table[rnum].johnson != quest->johnson)
   {
     ojn = real_mobile(quest_table[rnum].johnson);
+    if (ojn < 0) {
+      char oopsbuf[5000];
+      snprintf(oopsbuf, sizeof(oopsbuf), "BUILD ERROR: Quest %ld had non-existent Johnson %ld.", quest_table[rnum].vnum, quest_table[rnum].johnson);
+      mudlog(oopsbuf, NULL, LOG_SYSLOG, TRUE);
+      return;
+    }
+
     njn = real_mobile(quest->johnson);
+    if (njn < 0) {
+      char oopsbuf[5000];
+      snprintf(oopsbuf, sizeof(oopsbuf), "BUILD ERROR: Quest %ld has non-existent new Johnson %ld.", quest_table[rnum].vnum, quest->johnson);
+      mudlog(oopsbuf, NULL, LOG_SYSLOG, TRUE);
+      return;
+    }
+
     if (mob_index[ojn].func == johnson) {
       mob_index[ojn].func = mob_index[ojn].sfunc;
       mob_index[ojn].sfunc = NULL;
