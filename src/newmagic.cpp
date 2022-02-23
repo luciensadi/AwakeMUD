bde515f03643b86b8c8b56e230b4ac7b49c63eb0
TheVilest <78279656+TheVilest@users.noreply.github.com>
2022-02-23 01:18:48 -0500

    Update newmagic.cpp

--- src/newmagic.cpp
+++ src/newmagic.cpp
@@ -1711,35 +1711,41 @@ void cast_health_spell(struct char_data *ch, int spell, int sub, int force, char
     case SPELL_NIGHTVISION:
       WAIT_STATE(ch, (int) (SPELL_WAIT_STATE_TIME));
       success = success_test(skill, 6 + target_modifiers);
-      if (success > 0 && !AFF_FLAGGED(ch, AFF_LOW_LIGHT)) {
-        create_sustained(ch, vict, spell, force, 0, success, spells[spell].draindamage);
+      if (success < 1 || AFF_FLAGGED(vict, AFF_LOW_LIGHT)) {
+        send_to_char(FAILED_CAST, ch);
+      } else {
+        AFF_FLAGS(vict).SetBit(AFF_LOW_LIGHT);
         send_to_char("Your eyes tingle as the shadows around you become clearer.\r\n", vict);
         act("You successfully sustain that spell on $N.", FALSE, ch, 0, vict, TO_CHAR);
-      } else
-        send_to_char(FAILED_CAST, ch);
-      spell_drain(ch, spell, force, 0);
+        create_sustained(ch, vict, spell, force, 0, success, drain);
+      }
+      spell_drain(ch, spell, force, drain);
       break;
     case SPELL_INFRAVISION:
       WAIT_STATE(ch, (int) (SPELL_WAIT_STATE_TIME));
       success = success_test(skill, 6 + target_modifiers);
-      if (success > 0 && !AFF_FLAGGED(ch, AFF_INFRAVISION)) {
-        create_sustained(ch, vict, spell, force, 0, success, spells[spell].draindamage);
+      if (success < 1 || AFF_FLAGGED(vict, AFF_INFRAVISION)) {
+        send_to_char(FAILED_CAST, ch);
+      } else {
+        AFF_FLAGS(vict).SetBit(AFF_INFRAVISION);
         send_to_char("Your eyes tingle as you begin to see heat signatures around you.\r\n", vict);
         act("You successfully sustain that spell on $N.", FALSE, ch, 0, vict, TO_CHAR);
-      } else
-        send_to_char(FAILED_CAST, ch);
-      spell_drain(ch, spell, force, 0);
+        create_sustained(ch, vict, spell, force, 0, success, drain);
+      }
+      spell_drain(ch, spell, force, drain);
       break;
     case SPELL_LEVITATE:
       WAIT_STATE(ch, (int) (SPELL_WAIT_STATE_TIME));
       success = success_test(skill, 4 + target_modifiers);
-      if (success > 0 && !AFF_FLAGGED(ch, AFF_LEVITATE)) {
-        create_sustained(ch, vict, spell, force, 0, success, spells[spell].draindamage);
+      if (success < 1 || AFF_FLAGGED(vict, AFF_LEVITATE)) {
+        send_to_char(FAILED_CAST, ch);
+      } else {
+        AFF_FLAGS(vict).SetBit(AFF_LEVITATE);
         send_to_char("Your feet gently lift off from the ground as you levitate.\r\n", vict);
         act("You successfully sustain that spell on $N.", FALSE, ch, 0, vict, TO_CHAR);
-      } else
-        send_to_char(FAILED_CAST, ch);
-      spell_drain(ch, spell, force, 0);
+        create_sustained(ch, vict, spell, force, 0, success, drain);
+      }
+      spell_drain(ch, spell, force, drain);
       break;
   }
 }
