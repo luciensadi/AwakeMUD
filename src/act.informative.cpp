c87dc14bc74fb8336a7decc5029c56cb1d3c173a
Lucien <luciensadi@gmail.com>
2022-06-25 17:44:11 -0700

    Fixed crashbug in drug withdrawal aff

--- src/act.informative.cpp
+++ src/act.informative.cpp
@@ -6244,7 +6244,7 @@ ACMD(do_status)
       printed = TRUE;
     }
     else if (GET_DRUG_ADDICT(targ, i) > 0) {
-      send_to_char(ch, "  ^y%s withdrawal^n\r\n", drug_types[i]);
+      send_to_char(ch, "  ^y%s withdrawal^n\r\n", drug_types[i].name);
     }
   }
 
