25455569f1995bf2e413f360ee9a3b9205c2679c
Lucien <luciensadi@gmail.com>
2022-07-12 14:15:05 -0700

    Fixed spacing issue with shop quantities.

--- src/newshop.cpp
+++ src/newshop.cpp
@@ -1408,7 +1408,7 @@ void shop_list(char *arg, struct char_data *ch, struct char_data *keeper, vnum_t
         i--;
         continue;
       }
-      snprintf(ENDOF(buf), sizeof(buf) - strlen(buf), " %2d)  ", i);
+      snprintf(ENDOF(buf), sizeof(buf) - strlen(buf), " %3d)  ", i);
       if (sell->type == SELL_ALWAYS || (sell->type == SELL_AVAIL && GET_OBJ_AVAILTN(obj) == 0))
         snprintf(ENDOF(buf), sizeof(buf) - strlen(buf), "Yes      ");
       else if (sell->type == SELL_AVAIL) {
