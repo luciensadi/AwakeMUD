f69e92bed4118f6a29e282fd3966f5f7aee4766a
Lucien <luciensadi@gmail.com>
2022-08-16 09:33:47 -0700

    Fixed bug with speech order in phone use

--- src/act.comm.cpp
+++ src/act.comm.cpp
@@ -1685,8 +1685,8 @@ ACMD(do_phone)
       strlcpy(voice, "A masked voice", VOICE_BUF_SIZE);
 
     snprintf(buf3, MAX_STRING_LENGTH, "^YYou say into your phone in %s, \"%s%s^Y\"\r\n",
-             capitalize(argument),
              skills[language].name,
+             capitalize(argument),
              ispunct(get_final_character_from_string(argument)) ? "" : ".");
 
     send_to_char(buf3, ch);
