d0fac13b0707f8f74ee31fff7ee86555cbaeeec4
jdevnull <37560372+jdevnull@users.noreply.github.com>
2023-01-01 00:49:10 -1000

    Add newbie death clarification message

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1687,9 +1687,15 @@ void command_interpreter(struct char_data * ch, char *argument, const char *tcna
         break;
       case POS_MORTALLYW:
         send_to_char("You are in a pretty bad shape! You can either wait for help, or give up by typing ^WDIE^n.\r\n", ch);
+        if (PLR_FLAGGED(ch, PLR_NEWBIE)) {
+          send_to_char(ch, "^L[OOC: While your TKE is less than 50, your only penalty for dying is losing your current job. See ^wHELP NEWBIE^L for more details.]\r\n");
+        }
         break;
       case POS_STUNNED:
         send_to_char("All you can do right now is think about the stars! You can either wait to recover, or give up by typing ^WDIE^n.\r\n", ch);
+        if (PLR_FLAGGED(ch, PLR_NEWBIE)) {
+          send_to_char(ch, "^L[OOC: While your TKE is less than 50, your only penalty for dying is losing your current job. See ^wHELP NEWBIE^L for more details.]\r\n");
+        }
         break;
       case POS_SLEEPING:
         send_to_char("In your dreams, or what?\r\n", ch);
