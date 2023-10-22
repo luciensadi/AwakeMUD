a85ab92f3fdc3a0c846e3808d460f2e741c536e2
Lucien <luciensadi@gmail.com>
2023-10-21 19:54:45 -0700

    Used regex with word boundaries to make helpfiles smarter

--- src/act.informative.cpp
+++ src/act.informative.cpp
@@ -5178,11 +5178,11 @@ void display_help(char *help, int help_len, const char *arg, struct char_data *c
     }
   }
 
-  // Second strategy: Search for possible like-matches.
-  snprintf(query, sizeof(query), "SELECT * FROM help_topic WHERE name LIKE '%%%s%%' ORDER BY name ASC", prepared_for_like);
+  // Second strategy: Search for possible like-matches where words start with the entered text.
+  snprintf(query, sizeof(query), "SELECT * FROM help_topic WHERE name REGEXP '.*[[:<:]]%s.*' ORDER BY name ASC", prepared_for_like);
   if (mysql_wrapper(mysql, query)) {
     // If we don't find it here either, we know the file doesn't exist-- failure condition.
-    snprintf(help, help_len, "No such help file exists.\r\n");
+    snprintf(help, help_len, "No such help file exists (error case).\r\n");
     return;
   }
   res = mysql_store_result(mysql);
