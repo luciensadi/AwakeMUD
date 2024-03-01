356eb0268553d9b8fd8d51c6944c6cf08ca23e70
Lucien <luciensadi@gmail.com>
2024-02-29 16:12:33 -0800

    Enabled HISTORY ALL and HISTORY ROLEPLAY

--- src/act.comm.cpp
+++ src/act.comm.cpp
@@ -2126,6 +2126,12 @@ void raw_message_history(struct char_data *ch, int channel, int quantity) {
     case COMM_CHANNEL_LOCAL:
       send_message_history_to_descriptor(ch->desc, channel, quantity, "seen locally");
       break;
+    case COMM_CHANNEL_ROLEPLAY:
+      send_message_history_to_descriptor(ch->desc, channel, quantity, "seen ICly");
+      break;
+    case COMM_CHANNEL_ALL:
+      send_message_history_to_descriptor(ch->desc, channel, quantity, "seen across all channels");
+      break;
     default:
       snprintf(buf, sizeof(buf), "SYSERR: Unrecognized channel/subcmd %d provided to raw_message_history's channel switch.", channel);
       mudlog(buf, NULL, LOG_SYSLOG, TRUE);
