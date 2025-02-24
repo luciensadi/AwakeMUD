56a2fe16ad6f5fca0816064134fac706d5db7e04
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2025-02-24 00:35:54 +0000

    Fixed complex form loading in the matrix

--- src/utils.cpp
+++ src/utils.cpp
@@ -5810,7 +5810,10 @@ struct obj_data *make_new_finished_program(int part_type, int mpcp, int rating=0
 }
 
 struct obj_data *make_otaku_deck(struct char_data *ch) {
-  struct obj_data *new_deck = read_object(OBJ_CUSTOM_CYBERDECK_SHELL, VIRTUAL, OBJ_LOAD_REASON_OTAKU_BS);
+  struct obj_data *asist, *new_deck;
+  FAILURE_CASE(!(asist = find_cyberware(ch, CYB_ASIST)), "You don't have an asist converter.");
+
+  *new_deck = read_object(OBJ_CUSTOM_CYBERDECK_SHELL, VIRTUAL, OBJ_LOAD_REASON_OTAKU_BS);
 
   // Add parts.
   int mpcp = (GET_REAL_INT(ch) + GET_REAL_WIL(ch) + GET_REAL_CHA(ch) + 2) / 3; // adding 2 always ensures a round up
@@ -5837,8 +5840,9 @@ struct obj_data *make_otaku_deck(struct char_data *ch) {
   new_deck->obj_flags.extra_flags.SetBit(ITEM_EXTRA_CONCEALED_IN_EQ);
   new_deck->obj_flags.extra_flags.SetBit(ITEM_EXTRA_OTAKU_BS);
 
-  for (struct obj_data *form = ch->carrying; form; form = form->next_content) {
+  for (struct obj_data *form = asist->contains; form; form = form->next_content) {
     if (GET_OBJ_TYPE(form) != ITEM_COMPLEX_FORM) continue;
+    if (GET_DESIGN_PROGRAMMING_TICKS_LEFT(form) > 0) continue; // The complex form is in progress.
     struct obj_data *active = read_object(OBJ_BLANK_PROGRAM, VIRTUAL, OBJ_LOAD_REASON_OTAKU_BS);
     GET_PROGRAM_TYPE(active) = GET_PROGRAM_TYPE(form);
     GET_PROGRAM_SIZE(active) = 1;
