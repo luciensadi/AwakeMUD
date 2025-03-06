a39ccbbe3d63c8fe06d380204fbe1896ac5e4eeb
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2025-03-06 03:01:32 +0000

    Added helpful message on channel skill spreads

--- src/spec_procs.cpp
+++ src/spec_procs.cpp
@@ -5701,7 +5701,7 @@ SPECIAL(chargen_skill_annex) {
         for (int csi=SKILL_CHANNEL_ACCESS; csi <= SKILL_CHANNEL_SLAVE;csi++) {
           if (csi == ci) continue; // skip checking the parent skill against itself.
           if (REAL_SKILL(ch, csi) == REAL_SKILL(ch, ci)) {
-            send_to_char(ch, "You cannot have more than one channel skill at %d for otaku at character generation. Please reduce the value of %s.\r\n",
+            send_to_char(ch, "You cannot have more than one channel skill at %d for otaku at character generation. Otaku channel skills cannot exceed 6, 5, 4, 3, 3 ratings in chargen. Please reduce the value of %s.\r\n",
               REAL_SKILL(ch, ci), skills[ci].name);
             return TRUE;
           }
