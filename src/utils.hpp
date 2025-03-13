5396dc55304eeb6edf4d03c0c3231d9de79bec52
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2025-03-13 21:32:58 +0000

    Moved con timer to work around connection cuts on learning complex forms

--- src/utils.hpp
+++ src/utils.hpp
@@ -1409,10 +1409,10 @@ bool is_weapon_focus_usable_by(struct obj_data *focus, struct char_data *ch);
 #define GET_COMPLEX_FORM_WOUND_LEVEL(prog)                  (GET_OBJ_VAL((prog), 2))
 #define GET_COMPLEX_FORM_KARMA_PAID(prog)                   (GET_OBJ_VAL((prog), 3))
 #define GET_COMPLEX_FORM_LEARNING_TICKS_LEFT(prog)          (GET_OBJ_VAL((prog), 4))
+#define GET_COMPLEX_FORM_ORIGINAL_TICKS_LEFT(prog)          (GET_OBJ_VAL((prog), 5)) /* Using this instead of timer because timer isn't persisted */
 #define GET_COMPLEX_FORM_SIZE(prog)                         (GET_OBJ_VAL((prog), 6))
 #define GET_COMPLEX_FORM_LEARNING_FAILED(prog)              (GET_OBJ_VAL((prog), 7))
 #define GET_COMPLEX_FORM_CREATOR_IDNUM(prog)                (GET_OBJ_VAL((prog), 9))
-#define GET_COMPLEX_FORM_ORIGINAL_TICKS_LEFT(prog)          (GET_OBJ_TIMER((prog)))
 
 // ITEM_GUN_AMMO convenience defines
 #define GET_AMMOBOX_QUANTITY(box)                           (GET_OBJ_VAL((box), 0))
