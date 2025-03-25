157abd7a98e3d58aae635ab8ec716eb6c38c6569
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2025-03-25 10:16:34 -0700

    Added GMCP Support with Protocols (#793)
    * Added GMCP Support with Protocols
    
    * Added GMCP Char.Status
    
    * Housekeeping and adding more functions
    
    * Fixed GMCP Negotiation
    
    * Added the ability to RECEIVE GMCP messages from the client.
    
    * Added GMCP Support for different submodule client calls
    
    * Removed extra pools logic
    
    * Moved movement code call
    
    * Fixing code checks
    
    * Expanded charpools
    
    * Streamlined exits code
    
    * Added GMCP Matrix Hooks
    
    * More GMCP updates
    
    * Updates to vitals
    
    * Fixing GMCP protocol newlines
    
    * Added various other helpful info
    
    * Removing coords check
    
    * Adding a few more hooks for updates
    
    * Added to option in gmcp
    
    * PR Fixes for GMCP

--- src/act.other.cpp
+++ src/act.other.cpp
@@ -42,6 +42,7 @@
 #include "moderation.hpp"
 #include "chipjacks.hpp"
 #include "player_exdescs.hpp"
+#include "gmcp.hpp"
 
 #ifdef GITHUB_INTEGRATION
 #include <curl/curl.h>
@@ -2374,6 +2375,8 @@ ACMD(do_treat)
 
   // Treater gets a wait state.
   WAIT_STATE(ch, 2 RL_SEC);
+
+  SendGMCPCharVitals(vict); // Update State
 }
 
 ACMD(do_astral)
